ab8aa947a86394f84f397331818306a597b6e89a
Jamie Francese <jamienf@gmail.com>
2014-12-06 12:11:10 -0500

    made keys file

--- lib/pig.rb
+++ lib/pig.rb
@@ -1,9 +1,12 @@
 class Pig
+  attr_accessor :x, :y, :move_up, :move_down, :move_right, :move_left, :pig_speed
+
   def initialize(window, x, y)
     @pig_image = Gosu::Image.new(window, 'img/pig.png')
     @x = x
     @y = y
 
+    @pig_speed = 50
 
     @move_up = false
     @move_down = false
@@ -20,7 +23,29 @@ class Pig
   end
 
   def update
+    if move_up
+      unless y <= 0
+        @y += -@pig_speed
+      end
+    end
 
+    if move_down
+      unless y >= (600 - @pig_image.height)
+        @y += @pig_speed
+      end
+    end
+
+    if move_right
+      unless x >= (800 - @pig_image.width)
+        @x += @pig_speed
+      end
+    end
+
+    if move_left
+      unless x <= 0
+        @x += -@pig_speed
+      end
+    end
   end
 
 end
