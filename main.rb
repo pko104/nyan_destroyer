74a58af738652a5f90fd23ba0d8354ab39892b7f
Spencer Dixon <spencercdixon@gmail.com>
2014-12-06 12:10:03 -0500

    Got grid system set up

--- main.rb
+++ main.rb
@@ -1,17 +1,20 @@
+require 'pry'
 require 'gosu'
+
+require_relative 'grid'
 
 class Game < Gosu::Window
   def initialize
     super(1000, 1000, false)
+    @grid = Grid.new(self)
   end
 
   def draw
-
+    @grid.draw
   end
 
   def update
 
   end
 end
-
 Game.new.show
